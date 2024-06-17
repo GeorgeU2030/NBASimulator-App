eb82a3ad19ea5d513cb89c122758af3d270cf456
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-17 16:22:44 -0500

    feat: animations and toast

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { ButtonModule } from 'primeng/button';
 
 import { Team } from '../interfaces/Team';
 import { TeamService } from '../services/team.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +16,10 @@ export class HomeComponent implements OnInit {
 
   listTeams : Team[] = [];
 
-  constructor (private teamService: TeamService){
+  constructor (
+    private teamService: TeamService,
+    private router: Router 
+  ){
   }
 
   getTeams(){
@@ -24,6 +28,10 @@ export class HomeComponent implements OnInit {
         this.listTeams = data;
       }
     );
+  }
+
+  navigate(){
+    this.router.navigate(['/seasons']);
   }
 
   ngOnInit(): void {
--- src/app/seasons/seasons.component.ts
+++ src/app/seasons/seasons.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-seasons',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, ToastModule],
   templateUrl: './seasons.component.html',
   styleUrl: './seasons.component.css'
 })
 export class SeasonsComponent {
+
+  constructor(
+    private message: MessageService
+  ) { }
+
+  newSeason(){
+    this.message.add({severity:'success', summary:'New Season', detail:'Season created successfully'});
+  }
 
 }
