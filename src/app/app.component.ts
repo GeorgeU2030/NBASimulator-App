b54c0e077f2c331068f4318ae7c2875270194c6b
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-17 14:21:08 -0500

    style: html style page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Team } from './interfaces/Team';
+import { TeamService } from './services/team.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,22 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'nbasimulator_app';
+export class AppComponent implements OnInit {
+  
+  listTeams : Team[] = [];
+
+  constructor (private teamService: TeamService){
+  }
+
+  getTeams(){
+    this.teamService.getTeams().subscribe(
+      data => {
+        this.listTeams = data;
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getTeams();
+  }
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Team } from '../interfaces/Team';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  private endPoint: string = environment.BACKEND_URL;
+  private apiUrl: string = this.endPoint + "Team";
+
+  constructor(private http:HttpClient) { }
+
+  getTeams():Observable<Team[]>{
+    return this.http.get<Team[]>(this.apiUrl);
+  }
+
 }
