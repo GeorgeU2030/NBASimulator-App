38a7310bb612438f13ec4b59a8ff2292fea67662
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-22 17:25:51 -0500

    refactor: async functions and order functions

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -11,6 +11,7 @@ import { Team } from '../interfaces/Team';
 import { TeamService } from '../services/team.service';
 import { matchnba } from '../logic/matchnba';
 import { MatchService } from '../services/match.service';
+import { Match } from '../interfaces/Match';
 
 @Component({
   selector: 'app-tournament',
@@ -24,9 +25,10 @@ export class TournamentComponent implements OnInit {
   listTeams : SeasonTeam[] = [];
   eastTeams : SeasonTeam[] = [];
   westTeams : SeasonTeam[] = [];
-  playinTeams : Team[] = [];
   playoffTeamsEast : Team[] = [];
   playoffTeamsWest : Team[] = [];
+  playInMatchesEast : Match[] = [];
+  playInMatchesWest : Match[] = [];
   season!: Season;
   seasonId!: number ;
   isOnPlayOffs = false;
@@ -56,7 +58,16 @@ export class TournamentComponent implements OnInit {
       this.westTeams = this.listTeams.filter(team => team.conference === 'Western');
       this.verifyPlayIn();
       this.verifyPlayOffs();
+      this.loadMatches();
     })
+  }
+
+  loadMatches(){
+    if(this.season.matches){
+      const sortedMatches = this.season.matches.sort((a, b) => a.matchId - b.matchId);
+      this.playInMatchesEast = sortedMatches.slice(0, 3);
+      this.playInMatchesWest = sortedMatches.slice(3, 6);
+    }
   }
 
   verifyPlayIn(){
@@ -89,90 +100,98 @@ export class TournamentComponent implements OnInit {
     
   }
 
-  async playIn(teams: SeasonTeam[]){
-    let loser:SeasonTeam, winner:SeasonTeam;
-
+  async getTeamByNameAsync(teamService: TeamService, name: string): Promise<Team> {
+    return new Promise((resolve, reject) => {
+      teamService.getTeamByName(name).subscribe({
+        next: resolve,
+        error: reject
+      });
+    });
+  }
+  
+  async createMatchAsync(matchService: MatchService, match: any): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      matchService.newMatch(match).subscribe({
+        next: () => resolve(),
+        error: (error) => reject(error)
+      });
+    });
+  }
+  
+  async playIn(teams: SeasonTeam[]): Promise<void> {
+    let loser: SeasonTeam, winner: SeasonTeam;
+  
     let scores = matchnba();
     let score1 = scores[0];
     let score2 = scores[1];
-
-    if(score1 > score2){
+  
+    if (score1 > score2) {
       loser = teams[1];
-    }else {
+    } else {
       loser = teams[0];
     }
+  
+    let team7 = await this.getTeamByNameAsync(this.teamService, teams[0].name);
+    let team8 = await this.getTeamByNameAsync(this.teamService, teams[1].name);
 
-    let team7:Team, team8:Team;
-
-    this.teamService.getTeamByName(teams[0].name).subscribe(data => {
-      team7 = data;
-      this.teamService.getTeamByName(teams[1].name).subscribe(data => {
-        team8 = data;
-        const match = {
-          matchId: 0,
-          seasonId: this.seasonId,
-          homeTeamId: team7.teamId,
-          visitorTeamId: team8.teamId,
-          homeScore: score1,
-          visitorScore: score2
-        }
-        this.matchService.newMatch(match).subscribe();
-      });
-    });
-
-    
-    let team9:Team, team10:Team;
-
+    let match = {
+      matchId: 0,
+      seasonId: this.seasonId,
+      homeTeamId: team7.teamId,
+      visitorTeamId: team8.teamId,
+      homeScore: score1,
+      visitorScore: score2
+    };
+  
+    await this.createMatchAsync(this.matchService, match);
+  
     scores = matchnba();
     score1 = scores[0];
     score2 = scores[1];
-
-    if(score1 > score2){
+  
+    if (score1 > score2) {
       winner = teams[2];
-    }else {
+    } else {
       winner = teams[3];
     }
+  
+    let team9 = await this.getTeamByNameAsync(this.teamService, teams[2].name);
+    let team10 = await this.getTeamByNameAsync(this.teamService, teams[3].name);
 
-    this.teamService.getTeamByName(teams[2].name).subscribe(data => {
-      team9 = data;
-      this.teamService.getTeamByName(teams[3].name).subscribe(data => {
-        team10 = data;
-        const match = {
-          matchId: 0,
-          seasonId: this.seasonId,
-          homeTeamId: team9.teamId,
-          visitorTeamId: team10.teamId,
-          homeScore: score1,
-          visitorScore: score2
-        }
-        this.matchService.newMatch(match).subscribe();
-      });
-    });
-
+    match = {
+      matchId: 0,
+      seasonId: this.seasonId,
+      homeTeamId: team9.teamId,
+      visitorTeamId: team10.teamId,
+      homeScore: score1,
+      visitorScore: score2
+    };
+  
+    await this.createMatchAsync(this.matchService, match);
+  
     scores = matchnba();
     score1 = scores[0];
     score2 = scores[1];
-    
+  
+    team9 = await this.getTeamByNameAsync(this.teamService, loser.name);
+    team10 = await this.getTeamByNameAsync(this.teamService, winner.name);
 
-    this.teamService.getTeamByName(loser.name).subscribe(data => {
-      team9 = data;
-      this.teamService.getTeamByName(winner.name).subscribe(data => {
-        team10 = data;
-        const match = {
-          matchId: 0,
-          seasonId: this.seasonId,
-          homeTeamId: team9.teamId,
-          visitorTeamId: team10.teamId,
-          homeScore: score1,
-          visitorScore: score2
-        }
-        this.matchService.newMatch(match).subscribe();
-      });
-    });
-
-    this.verifyPlayIn();
-    this.verifyPlayOffs();
-
+    match = {
+      matchId: 0,
+      seasonId: this.seasonId,
+      homeTeamId: team9.teamId,
+      visitorTeamId: team10.teamId,
+      homeScore: score1,
+      visitorScore: score2
+    };
+  
+    await this.createMatchAsync(this.matchService, match);
+  
+  }
+  
+  async generateMatches(eastTeams: SeasonTeam[], westTeams: SeasonTeam[]){
+    await this.playIn(eastTeams);
+    await this.playIn(westTeams);
   }
 
   startPlayIn(){
@@ -180,13 +199,15 @@ export class TournamentComponent implements OnInit {
     const playInEast = this.eastTeams.slice(6, 10);
     const playInWest = this.westTeams.slice(6, 10);
 
-    this.playIn(playInEast);
-    this.playIn(playInWest);
-    
+    this.generateMatches(playInEast, playInWest);
+
     this.isOnPlayIn = false;
     this.isOnPlayOffs = true;
 
     this.cdr.detectChanges();
+
+    this.loadMatches();
+
   }
 
   ngOnInit() {
